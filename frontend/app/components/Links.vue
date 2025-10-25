87bf67fe0d549d52088e35226fc151634de4955c
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2025-10-25 22:09:04 +0300

    feat: add Links and Notes components

---
+++ frontend/app/components/Links.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div 
+        class="
+            w-100 h-full shrink-0 
+            bg-amber-400 
+            rounded-2xl p-4
+        ">
+        <div 
+            class="
+                text-xl font-semibold
+                border-b-2 border-gray-950
+            ">
+            LINKS
+        </div>
+        <ul 
+            class="
+                pt-2
+            ">
+            <li class="w-full bg-green-500 p-2 rounded-md flex gap-2">
+                <div class="w-10 h-10 bg-red-800"></div>
+                <a href="#" class="text-blue-500 hover:underline">Link 1</a>
+            </li>
+            <li>
+                <a href="#" class="text-blue-500 hover:underline">Link 2</a>
+            </li>
+            <li>
+                <a href="#" class="text-blue-500 hover:underline">Link 3</a>
+            </li>
+        </ul>
+    </div>
+</template>
